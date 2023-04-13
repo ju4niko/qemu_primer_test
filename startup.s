1797790f986856393ac2124581f3e091ca98fab7
jmontenegro <jmontenegro@frba.utn.edu.ar>
2023-04-13 13:17:20 -0300

    comentarios agregados

--- startup.s
+++ startup.s
@@ -1,7 +1,11 @@
 
 _start:
-	LDR 	r2, str1
+	LDR 	r2, str1		# carga r2 con la var str1
+	MRC 	p15, 0, R1, c0, c0, 0	# move to reg from coprocessor:
+					# lee registro de estado que indica 
+					# modelo de arquitectura 
 	b 	.
-str1: 	.word 0xAA55AA55
+	
+str1: 	.word 	0xAA55AA55
 
 	
